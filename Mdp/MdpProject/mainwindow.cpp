01edf075c0d795284b7bda00ee8a8409b17a1f10
Boris Mitioglov <ibmttz13.5@gmail.com>
2014-05-18 19:38:02 +0400

    комбобокс исправленный

--- Mdp/MdpProject/mainwindow.cpp
+++ Mdp/MdpProject/mainwindow.cpp
@@ -12,7 +12,8 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    ui->progressBar->setValue(100);
+    ui->comboBox_2->setEnabled(false);
+    ui->pushButton->setEnabled(false);
 }
 
 
@@ -25,22 +26,68 @@ void MainWindow::on_pushButton_clicked()
 {
     Dialog *d = new Dialog();
     DBInfoGetter* dbinfogetter = new DBInfoGetter();
-    dbinfogetter->getAppropriateCountries(ui->comboBox->currentText(), "Страна");
-//    dbinfogetter->getFlightMatrix("Страна");
-//    DBInfoGetterDAO::printFlightMatrix(dbinfogetter->getFlightMatrix("Деревня").getMatrix());
+//    dbinfogetter->getAppropriateCountries(ui->comboBox->currentText(), "Страна");
 
-    graph<Village>* graph1 = new graph<Village>();
-    qDebug() << "Before print";
-    DBInfoGetterDAO::printFlightMatrix(graph1->getFlightMatrix().getMatrix());
+
+    if (ui->comboBox->currentText() == "По странам"){
+        graph<Country>* graph1 = new graph<Country>();
+        DBInfoGetterDAO::printFlightMatrix(graph1->getFlightMatrix().getMatrix());
+        d -> show();
+    }
+    else
+    if (ui->comboBox->currentText()=="По деревням"){
+        graph<Village>* graph1 = new graph<Village>();
+        DBInfoGetterDAO::printFlightMatrix(graph1->getFlightMatrix().getMatrix());
+        d -> show();
+    }
+    else
+    if (ui->comboBox->currentText()=="По городам"){
+            graph<City>* graph1 = new graph<City>();
+            DBInfoGetterDAO::printFlightMatrix(graph1->getFlightMatrix().getMatrix());
+            d -> show();
+    }
+    else
+    {
+        ui->label_4->setText("Выберите тип путешествия, пожалуйста");
+    }
 
 //    DBInfoGetterDAO::printFlightMatrix(dbinfogetter->getFlightMatrix("Страна").getMatrix());
-    d -> show();
+
 }
 
 void MainWindow::on_comboBox_currentTextChanged()
 {
-//   prop = ui->comboBox->currentText();
-   //QMessageBox::information(this, "123", country);
+    ui->label_4->clear();
+    ui->pushButton->setEnabled(true);
+    ui->comboBox_2->clear();
+    qDebug()<< ui->comboBox->currentText();
+    if (ui->comboBox->currentText() == "По странам"){
+        ui->comboBox_2->setEnabled(true);
+        ui->comboBox_2->addItem("Австралия");
+        ui->comboBox_2->addItem("Америка");
+        ui->comboBox_2->addItem("Англия");
+        ui->comboBox_2->addItem("Беларусь");
+        ui->comboBox_2->addItem("Болгария");
+        ui->comboBox_2->addItem("Германия");
+        ui->comboBox_2->addItem("Греция");
+        ui->comboBox_2->addItem("Испания");
+        ui->comboBox_2->addItem("Казахстан");
+        ui->comboBox_2->addItem("Норвегия");
+        ui->comboBox_2->addItem("Россия");
+    }
+    else
+    if (ui->comboBox->currentText()=="По деревням"){
+        ui->comboBox_2->setEnabled(true);
+        ui->comboBox_2->addItem("Куршавель");
+        ui->comboBox_2->addItem("Сент-Сирк");
+    }
+    else
+    {
+        if (ui->comboBox->currentText()=="По городам"){
+            ui->comboBox_2->setEnabled(true);
+        }
+    }
+
 }
 
 void MainWindow::on_comboBox_2_currentTextChanged()
